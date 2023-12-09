57487de70e7a0c7171901dbcbc2b4bd0df5b22bd
Charles Leclerc <leclerc.charles@gmail.com>
2023-12-09 15:58:35 +0100

    day 9 p1 14:22.61 p2 17:40.62

---
+++ day-009a.py
@@ -0,0 +1,19 @@
+t = 0
+
+for l in [a.strip() for a in open('day-009.input', 'r').readlines()]:
+    rows = []
+    cr = [int(a) for a in l.split()]
+    rows.append(cr)
+    while not all(v == 0 for v in cr):
+        nr = [cr[i+1] - cr[i] for i in range(0, len(cr)-1)]
+        rows.append(nr)
+        cr = nr
+    for i, r in reversed(list(enumerate(rows))):
+        if i == len(rows) - 1:
+            r.append(0)
+        else:
+            r.append(r[-1] + rows[i+1][-1])
+    t += rows[0][-1]
+
+print(t)
+
---
+++ day-009b.py
@@ -0,0 +1,19 @@
+t = 0
+
+for l in [a.strip() for a in open('day-009.input', 'r').readlines()]:
+    rows = []
+    cr = [int(a) for a in l.split()]
+    rows.append(cr)
+    while not all(v == 0 for v in cr):
+        nr = [cr[i+1] - cr[i] for i in range(0, len(cr)-1)]
+        rows.append(nr)
+        cr = nr
+    for i, r in reversed(list(enumerate(rows))):
+        if i == len(rows) - 1:
+            r.insert(0, 0)
+        else:
+            r.insert(0, r[0] - rows[i+1][0])
+    t += rows[0][0]
+
+print(t)
+
