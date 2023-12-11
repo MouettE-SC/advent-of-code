8c396228ed977f8301c4a0820f9dffdb6858c5dc
Charles Leclerc <leclerc.charles@gmail.com>
2023-12-11 15:40:55 +0100

    day 11 p1 19:34.33 p2 1:02:35.36

---
+++ day-011a.py
@@ -0,0 +1,30 @@
+from itertools import combinations
+
+grid = []
+
+for l in [a.strip() for a in open('day-011.input', 'r').readlines()]:
+    grid.append([c for c in l])
+    if l.count('.') == len(l):
+        grid.append([c for c in l])
+
+ca = []
+for j in range(0, len(grid[0])):
+    col = "".join([grid[i][j] for i in range(0, len(grid))])
+    if col.count('.') == len(grid):
+        ca.append(j)
+
+for j in reversed(ca):
+    for i in range(0, len(grid)):
+        grid[i].insert(j, '.')
+
+g = []
+for i in range(0, len(grid)):
+    for j in range(0, len(grid[i])):
+        if grid[i][j] == '#':
+            g.append((i, j))
+
+t = 0
+for (i, j), (a, b) in combinations(g, 2):
+    t += abs(i-a) + abs(j-b)
+
+print(t)
